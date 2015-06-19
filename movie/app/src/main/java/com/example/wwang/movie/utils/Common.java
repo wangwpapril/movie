e26f0e7afcfc93d31af3d066f34e6ac5edf4bdcb
Will Wang <wwang@peakcontact.com>
2015-06-19 11:54:14 +0800

    fix to crash issue

--- movie/app/src/main/java/com/example/wwang/movie/utils/Common.java
+++ movie/app/src/main/java/com/example/wwang/movie/utils/Common.java
@@ -67,7 +67,7 @@ public class Common {
 
 		dialogLoading.setIndeterminate(false);
 
-		dialogLoading.setCancelable(true);
+		dialogLoading.setCancelable(false);
 
 		dialogLoading.setOnDismissListener(listenerDismissLoading);
 
